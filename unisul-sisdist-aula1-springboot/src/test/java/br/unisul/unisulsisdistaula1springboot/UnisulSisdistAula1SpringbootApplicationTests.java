d2d8b12830837bcfb74f7d2469fd50d0452ffc22
jwernner <jorge.werner@gmail.com>
2022-11-22 00:28:13 -0300

    upload inicial
    upload inicial
    
    Co-Authored-By: jwernner <8781474+jwernner@users.noreply.github.com>

---
+++ unisul-sisdist-aula1-springboot/src/test/java/br/unisul/unisulsisdistaula1springboot/UnisulSisdistAula1SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unisul.unisulsisdistaula1springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnisulSisdistAula1SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
